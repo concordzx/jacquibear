8f40503a3a30a0335a0a85620dafd6213f6411fa
Conrad Lesiak <kaisel_inc@yahoo.com>
2014-09-08 21:52:10 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jacquibear::Application.config.secret_key_base = '0442d7443211ff225ca8e7aca61cef51ff887adaca03baac20fef3c4f56aef544af801dbf410ec8b29058a39bf49a55834cbfeb5555bcf0f211b77406787e856'
